877333121c5402004b23fa48d4422593a733d3b3
Peter Marcelis <peter.marcelis@hva.nl>
2019-06-26 10:56:47 +0200

    added changes, fixed last room bug

---
+++ Assets/SpawnEntranceExit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEntranceExit : MonoBehaviour {
+
+	bool shouldSpawn;
+	
+	private void Start() {
+		if(shouldSpawn){
+			Debug.Log("Entrance should be spawned");
+		}
+	}
+}
